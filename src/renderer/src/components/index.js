82057c47f5142e92c3883edfd10ce7760e68c51a
Fayaz K <info@fayazk.com>
2025-03-20 09:42:50 +0500

    ✨ (renderer): create index.js file to export components

---
+++ src/renderer/src/components/index.js
@@ -0,0 +1,11 @@
+// Export layout components
+export { default as AppLayout } from './AppLayout';
+
+// Export common components
+export { 
+  Button,
+  Card,
+  Input,
+  Sidebar,
+  TabNavigation
+} from './common';
